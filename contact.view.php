b970300e96e472d99ee875e40448f48b89a49e6e
irfan87 <nerve2009@yahoo.com>
2023-08-25 21:39:09 +0800

    actual pages with tailwindcss

--- contact.view.php
+++ contact.view.php
@@ -4,7 +4,7 @@
 <head>
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Home</title>
+	<title>Contact Us</title>
 	<script src="http://cdn.tailwindcss.com"></script>
 </head>
 
@@ -29,13 +29,12 @@
 						<div class="hidden md:block">
 							<div class="ml-10 flex items-baseline space-x-4">
 								<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-								<a href="/" class="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
-									aria-current="page">Home</a>
+								<a href="/" class=" text-white px-3 py-2 text-sm font-medium" aria-current="page">Home</a>
 								<a href="/about.php"
 									class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">About
 									Us</a>
 								<a href="/contact.php"
-									class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact
+									class="bg-gray-900 text-white rounded-md text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">Contact
 									Us</a>
 							</div>
 						</div>
@@ -166,13 +165,12 @@
 
 		<header class="bg-white shadow">
 			<div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-				<h1 class="text-3xl font-bold tracking-tight text-gray-900">Home</h1>
+				<h1 class="text-3xl font-bold tracking-tight text-gray-900">Contact Us</h1>
 			</div>
 		</header>
 		<main>
 			<div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
-				<h2 class="font-lg">Hello, there</h2>
-				<p>Welcome to the homepage</p>
+				<h2 class="font-lg">You are now at the contact us page!</h2>
 			</div>
 		</main>
 	</div>
--- index.php
+++ index.php
@@ -1,25 +1,4 @@
 <?php
 
-$business = [
-	'name' => 'laracasts',
-	'costs' => 30,
-	'categories' => ['Testing', 'PHP', 'JavaScript', 'Mobile Development', 'Web Development']
-];
-
-if ($business['costs'] > 20) {
-	echo 'not interested<br />';
-}
-
-foreach ($business['categories'] as $category) {
-	echo $category . ' ';
-}
-
-function register($user)
-{
-	// create the user record and save in the database
-	// log / sign in
-	// send a welcome email
-	// redirect to the new dashboard
-}
 
 require('index.view.php');
