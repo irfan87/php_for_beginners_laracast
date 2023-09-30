b281b48aceff96fd42161d9f41e44a2464f55699
irfan87 <nerve2009@yahoo.com>
2023-09-30 16:02:23 +0800

    other user are forbidden to view the other authorize user

--- demo/controllers/note.php
+++ demo/controllers/note.php
@@ -4,8 +4,18 @@ $config = require('config.php');
 $db = new Database($config['database']);
 
 $heading = 'Notes';
+$currentUserID = 3;
 
 $note = $db->queryOne('SELECT * FROM notes WHERE id=:id', ['id' => $_GET['id']])->fetch();
 
+if (!$note) {
+	abort();
+}
+
+// assigned the current user's name
+
+if ($note['user_id'] !== $currentUserID) {
+	abort(Response::FORBIDDEN);
+}
 
 require('views/note.views.php');
---
+++ demo/views/403.php
@@ -0,0 +1,11 @@
+<?php require('partials/head.php') ?>
+<?php require('partials/nav.php') ?>
+<main>
+	<div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+		<h2 class="text-2xl font-bold">You are not the author of this page.</h2>
+		<p class="mt-4">
+			<a href="/" class="text-blue underline">Home</a>
+		</p>
+	</div>
+</main>
+<?php require('partials/footer.php') ?>
